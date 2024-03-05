74ecd97f56e5c86c373dc374c200874c60dba000
zavialova-221-365 <146123114+zavialova-221-365@users.noreply.github.com>
2024-03-05 15:53:53 +0300

    DOMContentLoaded

---
+++ js/main.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const routesTableBody = document.getElementById('routesTableBody');
+    const pagination = document.getElementById('pagination');
+    const guidesTableBody = document.getElementById('guidesTableBody');
+    let currentPage = 1;
+    const itemsPerPage = 10; // Определение количества записей на странице
+    let allRoutesData = []; // Данные о всех маршрутах
+
+});
