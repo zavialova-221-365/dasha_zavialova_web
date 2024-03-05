31b52aa5ab9cbcf855785a4cd6625584f6e59155
zavialova-221-365 <146123114+zavialova-221-365@users.noreply.github.com>
2024-03-05 16:16:03 +0300

    getRoutesData

--- js/main.js
+++ js/main.js
@@ -5,5 +5,17 @@ document.addEventListener('DOMContentLoaded', function () {
     let currentPage = 1;
     const itemsPerPage = 10; // Определение количества записей на странице
     let allRoutesData = []; // Данные о всех маршрутах
+    function getRoutesData() {
+        const apiUrl = `http://exam-2023-1-api.std-900.ist.mospolytech.ru/api/routes?api_key=3990d76f-4908-438c-a2e9-90a0a642eb96`;
 
+        fetch(apiUrl)
+            .then(response => response.json())
+            .then(data => {
+                allRoutesData = data;
+                updateTable(currentPage);
+            })
+            .catch(error => {
+                console.error('Ошибка получения данных о маршрутах:', error);
+            });
+    }
 });
