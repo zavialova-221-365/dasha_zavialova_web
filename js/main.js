89279c8378fe4b14dc6e84c5078cdc768f5eb3e3
zavialova-221-365 <146123114+zavialova-221-365@users.noreply.github.com>
2024-03-05 16:41:37 +0300

    filteredRoutes.forEach

--- js/main.js
+++ js/main.js
@@ -18,4 +18,54 @@ document.addEventListener('DOMContentLoaded', function () {
                 console.error('Ошибка получения данных о маршрутах:', error);
             });
     }
+    const searchInput = document.getElementById('searchInput');
+    const applyButton = document.querySelector('.btn-primary');
+
+    applyButton.addEventListener('click', function () {
+        event.preventDefault(); // Предотвращаем действие по умолчанию - перезагрузку страницы
+
+        const searchTerm = searchInput.value.toLowerCase(); // Получаем введенный текст из поля поиска
+
+        // Фильтруем данные по введенному тексту
+        const filteredRoutes = allRoutesData.filter(route => {
+            return route.name.toLowerCase().includes(searchTerm);
+        });
+
+});
+// Очищаем и обновляем таблицу с отфильтрованными данными
+routesTableBody.innerHTML = '';
+filteredRoutes.forEach(route => {
+    const row = document.createElement('tr');
+    const nameCell = document.createElement('td');
+    const descriptionCell = document.createElement('td');
+    const objectsCell = document.createElement('td');
+    const buttonCell = document.createElement('td');
+    const selectButton = document.createElement('button');
+
+    nameCell.textContent = route.name;
+    descriptionCell.textContent = route.description;
+    objectsCell.textContent = route.mainObject;
+
+    selectButton.textContent = 'Выбрать';
+    selectButton.classList.add('btn', 'btn-secondary', 'selectRouteBtn');
+    selectButton.addEventListener('click', function () {
+        row.classList.toggle('selected');
+        const routeId = route.id;
+        const routeName = route.name;
+        loadGuidesForRoute(routeId, routeName);
+        updateSelectedRoute(routeName);
+        populateLanguagesSelect(routeId);
+    });
+
+    buttonCell.appendChild(selectButton);
+
+    row.appendChild(nameCell);
+    row.appendChild(descriptionCell);
+    row.appendChild(objectsCell);
+    row.appendChild(buttonCell);
+
+    routesTableBody.appendChild(row);
+});
+
+// Пересчитываем и создаем пагинацию для отфильтрованных данных
 });
